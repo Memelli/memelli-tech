a02c915f5b1e3fc8b209be4a435406c2656079ae
Julio Memeli <memeli>
2021-07-02 08:12:42 -0300

    hot fix build

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useQuery } from 'react'
 import axios from 'axios'
 import './repositories.css'
 import moment from 'moment'
